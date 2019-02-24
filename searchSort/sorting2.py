32cf2e0d7adaaabe84511f62e3b08317695c07ee
a-yasinsky <anatoly@yasinsky.pro>
2019-02-24 14:45:49 +0600

    add quick sort

--- searchSort/sorting2.py
+++ searchSort/sorting2.py
@@ -34,3 +34,30 @@ def mergeSort(alist):
 alist = [54,26,93,17,77,31,44,55,20]
 mergeSort(alist)
 print(alist)
+
+
+def switch(array, element, pivot):
+    pivot_val = array[pivot]
+    array[pivot] = array[element]
+    array[element] = array[pivot - 1]
+    array[pivot - 1] = pivot_val
+    return 0, pivot - 1
+
+def quicksort(array):
+    if(len(array) <= 1):
+        return array
+    pivot = len(array)-1
+    element = 0
+    array1 = []
+    array2 = []
+    while element < pivot :
+        if(array[element] > array[pivot]):
+            element, pivot = switch(array, element, pivot)
+        else:
+            element += 1
+    array1 = quicksort(array[0:pivot])
+    array2 = quicksort(array[pivot + 1:])
+    return array1 + [array[pivot]] + array2
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+print(quicksort(test))
